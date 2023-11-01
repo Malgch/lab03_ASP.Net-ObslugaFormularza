1f749e9aa2ce83c2e56f2f468d4e530fbd89fd29
Małgorzata Chudy <malgorzata.chudy@microsoft.wsei.edu.pl>
2023-11-01 22:33:48 +0100

    initial commit

---
+++ lab03_ASP.Net-ObslugaFormularza/Models/Data.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lab03_ASP.Net_ObslugaFormularza.Models
+{
+    public class Data
+    {
+        [Required(ErrorMessage = "Please fill your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please fill your e-mail address")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+        
+        
+    }
+}
+
+/* Imię (wymagane, minimum 2 znaki)
+• Nazwisko (wymagane, minimum 2 znaki)
+• Adres e-mail (wymagane, poprawny adres e-mail)
+• Hasło (wymagane, minimum 8 znaków, co najmniej jedna cyfra, jedna duża litera i jedna
+mała litera)
+• Potwierdzenie hasła (wymagane, musi być takie samo jak pole hasło)
+• Nr telefonu (odpowiedni typ)
+• Wiek (powyżej 10 lat do 80 lat)
+• Miasto (wybór z 5 propozycji) */ 
---
+++ lab03_ASP.Net-ObslugaFormularza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lab03_ASP.Net_ObslugaFormularza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
